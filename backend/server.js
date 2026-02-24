db1b281703709183b4763c8727e93a22ac0e1248
Ayush Agarwal <agarwalayush0604@gmail.com>
2026-02-24 12:15:51 +0530

    fixed cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5173'] }));
+app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5173', 'https://task-manager-app-ruddy-mu.vercel.app'] }));
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
